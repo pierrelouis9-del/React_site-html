bf1f6f8b814c97b0fefee35b99133590e3998ae7
Juvensky Pierre Louis <pierrelouisjuvensky5@gmail.com>
2026-06-19 17:06:43 -0300

    primeiro commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header(){
+    return(
+
+        <>
+         <h1>Lanchonete</h1>
+         <h2> O melhor do mundo </h2>
+        </>
+       
+       
+    )
+}
+
+export default Header
---
+++ src/components/Login.jsx
@@ -0,0 +1,17 @@
+function Login (){
+    return(
+        <div>
+        <>
+        <h1> yo </h1>
+        <p>lembre-se não pode ter duas <strong> div ou tags vazio separados sem sem eles mesmo dentro de um div ou tag </strong></p>
+        </>
+        <>
+        </>
+
+        </div>
+
+      
+        
+    )
+}
+export default Login
